6eb22b056a41e9fa36486563b2576b9460d58ffb
rnick1 <rnick0260@gmail.com>
2021-03-29 10:56:23 -0700

    Worked on server.

--- server.js
+++ server.js
@@ -5,16 +5,13 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const expressSession = require('express-session');
-const SessionStore = require('express-session-sequelize')(expressSession.Store);
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
-const hbs = exphbs.create({ helpers });
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -28,30 +25,15 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
-// var myStore = new SequelizeStore({
-//   db: sequelize,
-// });
-// app.use(
-//   session({
-//     secret: "keyboard cat",
-//     store: myStore,
-//     resave: false,
-//     proxy: true,
-//   })
-// );
-
-// myStore.sync();
